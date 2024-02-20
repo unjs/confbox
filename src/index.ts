bf48062486429ce6da1a0f39b6407497381f8b69
Pooya Parsa <pooya@pi0.io>
2024-02-20 20:07:07 +0100

    chore: update automd

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export * from "./json5";
-export * from "./jsonc";
-export * from "./yaml";
-export * from "./toml";
+export { parseJSON5 } from "./json5";
+export type { JSON5ParseOptions, JSON5StringifyOptions } from "./json5";
+
+export { parseJSONC } from "./jsonc";
+export type { JSONCParseError, JSONCParseOptions } from "./jsonc";
+
+export { parseYAML, stringifyYAML } from "./yaml";
+export type { parseYAMLOptions, stringifyYAMLOptions } from "./yaml";
+
+export { parseTOML } from "./toml";
